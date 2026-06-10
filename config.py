2acac740fe56f2102dfb26814a4a603f74455c2c
Elavarasi Arumugam <elavarasiarasiofficial@gmail.com>
2026-06-10 15:05:15 +0530

    Added deployment configuration

--- config.py
+++ config.py
@@ -2,6 +2,15 @@ import os
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'soil.db')}"
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        f"sqlite:///{os.path.join(BASE_DIR, 'soil.db')}"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "your-secret-key"
+
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "your-secret-key"
+    )
